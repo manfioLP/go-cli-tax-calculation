c674769ce5632f33a2a95aac35bf525a18d63336
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2023-06-15 20:45:08 -0300

    add utils package

--- utils/tax-calculator.go
+++ utils/tax-calculator.go
@@ -1,1 +1,62 @@
 package utils
+
+type Order struct {
+	Operation string  `json:"operation"`
+	UnitCost  float64 `json:"unit-cost"`
+	Quantity  int     `json:"quantity"`
+	Tax       float64 `json:"tax,omitempty"`
+}
+
+type TaxResponse struct {
+	Tax float64 `json:"tax"`
+}
+
+func calculateWeightedAverage(currentWeightedAverage float64, currentQuantity int, purchasePrice float64, purchasedQuantity int) float64 {
+	return ((float64(currentQuantity) * currentWeightedAverage) + (float64(purchasedQuantity) * purchasePrice)) / float64(currentQuantity+purchasedQuantity)
+}
+
+func CalculateTax(orders []Order) []TaxResponse {
+	const taxThreshold = 20000.00
+
+	buy := func(op Order) bool {
+		return op.Operation == "buy"
+	}
+
+	sell := func(op Order) bool {
+		return op.Operation == "sell"
+	}
+
+	calculateWeightedAverage := func(currentWeightedAverage float64, currentQuantity int, purchasePrice float64, purchasedQuantity int) float64 {
+		return (float64(currentQuantity) * currentWeightedAverage) + (float64(purchasedQuantity)*purchasePrice)/float64(currentQuantity+purchasedQuantity)
+	}
+
+	calculateProfit := func(sellPrice, weightedAverage float64, quantity int) float64 {
+		return ((sellPrice - weightedAverage) * float64(quantity)) * 0.2
+	}
+
+	var weightedAverage float64
+	var quantity int
+	taxResults := make([]TaxResponse, 0)
+
+	for _, op := range orders {
+		if buy(op) {
+			weightedAverage = calculateWeightedAverage(
+				weightedAverage,
+				quantity,
+				op.UnitCost,
+				op.Quantity,
+			)
+			quantity += op.Quantity
+			taxResults = append(taxResults, TaxResponse{Tax: 0.0})
+		} else if sell(op) { // sell
+			tax := 0.0
+			quantity -= op.Quantity
+			if op.UnitCost*float64(op.Quantity) > taxThreshold {
+				tax = calculateProfit(op.UnitCost, weightedAverage, op.Quantity)
+			}
+			taxResults = append(taxResults, TaxResponse{Tax: tax})
+		}
+	}
+
+	return taxResults
+}
--- utils/tax-calculator_test.go
+++ utils/tax-calculator_test.go
@@ -1,1 +1,81 @@
 package utils
+
+import (
+	"testing"
+)
+
+func TestCalculateWeightedPrice(t *testing.T) {
+	w := calculateWeightedAverage(0.0, 0, 20.00, 10)
+	expected := 20.0
+	if eq := w == expected; !eq {
+		t.Errorf("Expected weighted average to be %f but got %f", expected, w)
+	}
+
+	w = calculateWeightedAverage(20.0, 5, 10.00, 5)
+	expected = 15.0
+	if eq := w == expected; !eq {
+		t.Errorf("Expected weighted average to be %f but got %f", expected, w)
+	}
+}
+
+func TestCalculateWeightedPrice2(t *testing.T) {
+	w := calculateWeightedAverage(0.0, 0, 10.00, 100)
+	expected := 10.0
+	if eq := w == expected; !eq {
+		t.Errorf("Expected weighted average to be %f but got %f", expected, w)
+	}
+
+	w = calculateWeightedAverage(w, 0, 10.00, 10000)
+	expected = 10.0
+	if eq := w == expected; !eq {
+		t.Errorf("Expected weighted average to be %f but got %f", expected, w)
+	}
+}
+
+func TestCalculateTax(t *testing.T) {
+	// Test case 1
+	input1 := []Order{
+		{Operation: "buy", UnitCost: 10.00, Quantity: 100},
+		{Operation: "sell", UnitCost: 15.00, Quantity: 50},
+		{Operation: "sell", UnitCost: 15.00, Quantity: 50},
+	}
+	expected1 := []TaxResponse{
+		{Tax: 0.00},
+		{Tax: 0.00},
+		{Tax: 0.00},
+	}
+	result1 := CalculateTax(input1)
+	compareResults(t, expected1, result1)
+
+	// Test case 2
+	input2 := []Order{
+		{Operation: "buy", UnitCost: 10.00, Quantity: 100},
+		{Operation: "sell", UnitCost: 15.00, Quantity: 50},
+		{Operation: "sell", UnitCost: 15.00, Quantity: 50},
+		{Operation: "buy", UnitCost: 10.00, Quantity: 10000},
+		{Operation: "sell", UnitCost: 20.00, Quantity: 5000},
+	}
+	expected2 := []TaxResponse{
+		{Tax: 0.00},
+		{Tax: 0.00},
+		{Tax: 0.00},
+		{Tax: 0.00},
+		{Tax: 10000.00},
+	}
+	result2 := CalculateTax(input2)
+	compareResults(t, expected2, result2)
+}
+
+func compareResults(t *testing.T, expected, result []TaxResponse) {
+	if len(expected) != len(result) {
+		t.Errorf("Expected %d operations, but got %d", len(expected), len(result))
+		return
+	}
+
+	for i := 0; i < len(expected); i++ {
+		eq := expected[i].Tax == result[i].Tax
+		if !eq {
+			t.Errorf("At index %d expected %f but got %f", i, expected[i].Tax, result[i].Tax)
+		}
+	}
+}
